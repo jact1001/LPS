c6618077da0392fc70f6462c39d1306a34423904
AlexanderAndrade.Net <alexander.andrade@gmail.com>
2021-09-17 16:27:08 -0500

    Solving SPL problems

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-/*import { HomeOneComponent } from './pages/homeone/homeone.component';
-import { HomeTwoComponent } from './pages/hometwo/hometwo.component';
-import { HomeThreeComponent } from './pages/homethree/homethree.component';*/
 import { HomeComponent } from './pages/home/home.component';
 
 const children = [
@@ -22,21 +19,6 @@ const children = [
 ];
 
 const routes: Routes = [
-  /*{
-    path: '',
-    component: HomeOneComponent,
-    children
-  },
-  {
-    path: '',
-    component: HomeTwoComponent,
-    children
-  },
-  {
-    path: '',
-    component: HomeThreeComponent,
-    children
-  },*/
   {
     path: '',
     component: HomeComponent,
