0b9961f7f84c064114a39b5027a3887318656a16
johnnychinchajoa <jhony.chinchajoa@avaldigitallabs.com>
2021-09-23 00:53:31 -0500

    :bento: se adiciona modulos para productos

---
+++ src/app/products/education/education-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'heroes', loadChildren: () => import('../spl-modules/heroes/heroes.module').then(m => m.HeroesModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
